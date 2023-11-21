e16af1672762a2cee9a23885bf8c76db833b2036
Martin-Luther Obahor <kingm4159@gmail.com>
2023-11-21 14:31:45 +0100

    Lesson 1 - UseState Hook

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -2,28 +2,28 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import * as React from 'react'
+import {useState} from 'react'
 
 function Greeting() {
-  // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+    const [name, setName] = useState('')
 
-  function handleChange(event) {
-    // 🐨 update the name here based on event.target.value
-  }
+    function handleChange(event) {
+        setName(event.target.value)
+    }
 
-  return (
-    <div>
-      <form>
-        <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
-      </form>
-      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
-    </div>
-  )
+    return (
+        <div>
+            <form>
+                <label htmlFor="name">Name: </label>
+                <input value={name} onChange={handleChange} id="name"/>
+            </form>
+            {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+        </div>
+    )
 }
 
 function App() {
-  return <Greeting />
+    return <Greeting/>
 }
 
 export default App
